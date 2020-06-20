70d24834834bd28d6dfbaea20319761f2eff0736
friedooo <friedlich3@gmail.com>
2020-06-20 13:16:15 +0300

    feat: add listener

--- js/main.js
+++ js/main.js
@@ -52,9 +52,10 @@ class view {
     }
 
 
-    snakeLength = 5;
+    snakeLength = 3;
     arrX = [];
     arrY = [];
+    key = 68;
     
     createSnake() {
         let startPoint = Math.floor(Math.sqrt(this.cells)/2)*this.size;
@@ -64,14 +65,14 @@ class view {
         
         for (let i = this.snakeLength; i > 0; i--)
             {
-                this.arrX.push(this.x /*- i * this.size*/);
-                this.arrY.push(this.y - i * this.size);
+                this.arrX.push(this.x - i * this.size);
+                this.arrY.push(this.y /*- i * this.size*/);
             }
 
         for (let i = 0; i < this.arrX.length; i++)
         {
-            // this.ctx.fillRect(this.arrX[i], this.y, this.size, this.size);
-            this.ctx.fillRect(this.x, this.arrY[i], this.size, this.size);
+            this.ctx.fillRect(this.arrX[i], this.y, this.size, this.size);
+            // this.ctx.fillRect(this.x, this.arrY[i], this.size, this.size);
         }
     }
 
@@ -79,27 +80,46 @@ class view {
 
     startMove() { 
 
-        //this.x -= this.size
-        this.y -= this.size
+        this.keyHandler();
+
+        this.x -= this.size;
+        //this.y -= this.size
 
         setInterval(() => {
             console.log(this.arrX);
-            
-
-            
+ 
             this.ctx.clearRect(this.arrX[0], this.arrY[0], this.size, this.size);
             this.ctx.strokeRect(this.arrX[0], this.arrY[0], this.size, this.size);
                 
-            this.y += this.size
-
-            if (this.x === this.width)
-            {
-                this.x = 0
-            }
-
-            if (this.y === this.height)
-            {
-                this.y = 0
+            switch(this.key) {
+                case 68: 
+                    this.x += this.size;
+                    if (this.x === this.width)
+                        {
+                            this.x = 0
+                        }
+                    break;
+                case 65:
+                        if (this.x === 0)
+                            {
+                                this.x = this.width;
+                            }
+                    this.x -= this.size;
+                    break;
+                case 87: 
+                    if (this.y === 0)
+                        {
+                            this.y = this.height;
+                        }
+                    this.y -= this.size;
+                    break;
+                case 83:  
+                this.y += this.size;  
+                    if (this.y === this.height)
+                        {
+                            this.y = 0
+                        }
+                    break;
             }
             
             
@@ -112,7 +132,32 @@ class view {
             this.ctx.fillRect(this.x, this.y, this.size, this.size);
             
 
-        },1000);
+        },300);
+    }
+
+    keyHandler(){
+        const vertical = [87, 83];
+        const horizontal = [68, 65];
+        window.addEventListener('keydown', (e) => {
+            if ((this.key === 87 || this.key === 83) && 
+            this.arrY[this.arrY.length - 1] != this.arrY[this.arrY.length - 2])
+            {
+                if (e.which === 68 || e.which === 65)
+                {
+                    this.key = e.which;
+                }
+            }
+
+            if ((this.key === 68 || this.key === 65) && 
+                this.arrX[this.arrX.length - 1] != this.arrX[this.arrX.length - 2])
+            {
+                if (e.which === 87 || e.which === 83)
+                {
+                    this.key = e.which;
+                }
+            }
+        });
+        
     }
 
 
