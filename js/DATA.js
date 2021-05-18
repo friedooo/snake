f1a540ab293eca06034269c1d33362a1e3b12dfb
Александр <54706207+friedooo@users.noreply.github.com>
2021-05-18 18:52:32 +0300

    feat: change inages url from game.js to data.js

--- js/DATA.js
+++ js/DATA.js
@@ -14,5 +14,20 @@ export default {
                 "настройки",
                 "рекорды"
             ]
-   }
+   },
+   "gameImages" : [
+    '../src/img/burger.svg',
+    '../src/img/apple.svg',
+    '../src/img/beer.svg',
+    '../src/img/pizza.svg',
+    '../src/img/donut.svg',
+    '../src/img/popcorn.svg',
+    '../src/img/bread.svg',
+    '../src/img/ice-cream.svg',
+    '../src/img/meat.svg',
+    '../src/img/chocolate.svg',
+    '../src/img/tomato.svg',
+    '../src/img/tea.svg',
+    '../src/img/carrot.svg'
+    ]
 }
--- js/game.js
+++ js/game.js
@@ -1,23 +1,11 @@
+import DATA from './DATA.js'
+
 export class Game {
     constructor(height, width, cells) {
         this.height = height;
         this.width = width;
         this.cells = cells;
-        this.IMAGES = [
-            '../src/img/burger.svg',
-            '../src/img/apple.svg',
-            '../src/img/beer.svg',
-            '../src/img/pizza.svg',
-            '../src/img/donut.svg',
-            '../src/img/popcorn.svg',
-            '../src/img/bread.svg',
-            '../src/img/ice-cream.svg',
-            '../src/img/meat.svg',
-            '../src/img/chocolate.svg',
-            '../src/img/tomato.svg',
-            '../src/img/tea.svg',
-            '../src/img/carrot.svg'
-        ];
+        this.IMAGES = DATA.gameImages;
         this.count = 0;
         this.snakeLength = 5;
         this.arrX = [];
