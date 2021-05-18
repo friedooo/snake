3372290211fbe76ffe95a2dd320af42a7aeb05b1
Александр <54706207+friedooo@users.noreply.github.com>
2021-05-18 15:13:05 +0300

    feat: make function default lang instead

--- js/main.js
+++ js/main.js
@@ -2,9 +2,12 @@ import {Game} from './game.js'
 import {ENGDATA, Menu} from './menu.js'
 import DATA from './DATA.js'
 
-if (!localStorage.getItem('lang')) {
-    localStorage.setItem('lang', 'eng');
-}
+(function() {
+    if (!localStorage.getItem('lang')) {
+        localStorage.setItem('lang', 'eng');
+    }
+    console.log(1);
+})
 
 
 let menu = new Menu();
